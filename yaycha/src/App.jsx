5cc9cc133d26dc735a01bb0612f897bd7be08a79
noahkhant <khantbhonenaing.007@gmail.com>
2025-03-31 20:12:45 +0630

    commit at 20250331 at 8:15

--- yaycha/src/App.jsx
+++ yaycha/src/App.jsx
@@ -2,27 +2,54 @@ import { useState } from "react";
 
 import List from "./List";
 import Item from "./Item";
+import Form from "./Form";
 
 const App = () => {
   const [data, setData] = useState([
-    {id: 1, content: "Hello World!", name: "Bob"},
-    {id: 2, content: "React is fun.", name: "Noah"},
-    {id: 3, content: "Yay, interesting.", name: "John"},
+    { id: 1, content: "Hello World!", name: "Bob" },
+    { id: 2, content: "React is fun.", name: "Noah" },
+    { id: 3, content: "Yay, interesting.", name: "John" },
   ]);
 
+  const [showForm, setShowForm] = useState(false);
+
   const remove = (id) => {
-    setData(data.filter(item => item.id !== id));
-  }
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+  };
 
   return (
-    <div style= {{maxWidth: 600, margin: "20px auto"}}>
-      <h1>Yaycha</h1>
+    <div style={{ maxWidth: 600, margin: "20px auto" }}>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}>
+        Yaycha
+        <button
+          onClick={() => setShowForm(!showForm)}
+          style={{
+            width: 32,
+            height: 32,
+            borderRadius: 50,
+            border: "0 none",
+            background: showForm ? "#dc3545" : "#0d6efd",
+            color: "white",
+          }}>
+          {showForm ? "×" : "+"}
+        </button>
+      </h1>
+
+      {showForm && <Form add={add} />}
       <List>
-          {data.map(item => {
-            return (
-              <Item key={item.id} item={item} remove={remove} />
-            )
-          })}
+        {data.map((item) => {
+          return <Item key={item.id} item={item} remove={remove} />;
+        })}
       </List>
     </div>
   );
