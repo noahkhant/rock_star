c3ff0423c7552d1e534c803f07fe6600f274e3fa
noahkhant <khantbhonenaing.007@gmail.com>
2025-05-21 22:26:20 +0630

    commit at 20250521 at 10:30 pm

--- yaycha/src/pages/Register.jsx
+++ yaycha/src/pages/Register.jsx
@@ -1,42 +1,77 @@
-import {
-    Alert,
-    Box,
-    Button,
-    TextField,
-    Typography
-} from "@mui/material";
+import { Alert, Box, Button, TextField, Typography } from "@mui/material";
 
-import { useNavigate } from "react-router-dom";
-import {useApp} from "../ThemedApp";
+import { useRef, useState } from "react";
+
+import { useMutation } from "@tanstack/react-query";
+import { postUser } from "../libs/fetcher";
+import { useAsyncError, useNavigate } from "react-router-dom";
+import { useApp } from "../ThemedApp";
 
 const Register = () => {
-    const navigate = useNavigate();
-    const {setAuth} = useApp();
+  const { setGlobalMsg } = useApp();
 
-    return(
-        <Box>
-            <Typography variant="h3">Register</Typography>
-            <Alert severity="warning" sx={{ mt: 2 }}>All field is required</Alert>
+  const nameInput = useRef();
+  const usernameInput = useRef();
+  const bioInput = useRef();
+  const passwordInput = useRef();
 
-            <form onSubmit={e => {
-                e.preventDefault();
-                setAuth(true);
-                navigate("/profile/1");
-            }}>
-                <Box
-                    sx={{ display: "flex", flexDirection: "column", gap: 1, mt: 2 }}>
-                    <TextField placeholder="Name" fullWidth/>
-                    <TextField placeholder="Username" fullWidth/>
-                    <TextField placeholder="Biography" fullWidth/>
-                    <TextField type="password" placeholder="Password" fullWidth/>
+  const [error, setError] = useState(null);
 
-                    <Button type="submit" variant="contained" fullWidth>
-                        Register
-                    </Button>
-                </Box>
-            </form>
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    const name = nameInput.current.value;
+    const username = usernameInput.current.value;
+    const bio = bioInput.current.value;
+    const password = passwordInput.current.value;
+
+    if (!name || !username || !bio || !password) {
+      setError("name, username and password required");
+      return false;
+    }
+
+    create.mutate({ name, username, bio, password });
+  };
+
+  const create = useMutation(async (data) => {
+    postUser(data),
+      {
+        onError: async () => {
+          setError("Can't create user");
+        },
+        onSuccess: async () => {
+          setGlobalMsg("Account created successfully");
+          navigate("/login");
+        },
+      };
+  });
+
+  return (
+    <Box>
+      <Typography variant="h3">Register</Typography>
+      {error && (
+        <Alert severity="warning" sx={{ mt: 2 }}>
+          {error}
+        </Alert>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1, mt: 2 }}>
+          <TextField inputRef={name} placeholder="Name" fullWidth />
+          <TextField inputRef={username} placeholder="Username" fullWidth />
+          <TextField inputRef={bio} placeholder="Bio" fullWidth />
+          <TextField inputRef={password} placeholder="Password" fullWidth />
         </Box>
-    );
+
+        <Button variant="contained" type="contained" fullWidth>
+          Register
+        </Button>
+      </form>
+    </Box>
+  );
 };
 
 export default Register;
