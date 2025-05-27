50801c3fc9d95f2b5e044501d1816bc40c6880b0
noahkhant <khantbhonenaing.007@gmail.com>
2025-05-27 21:38:08 +0630

    commit at 2025/05/27 and 9:40 pm

--- yaycha/src/pages/Login.jsx
+++ yaycha/src/pages/Login.jsx
@@ -1,41 +1,80 @@
-import {
-    Alert,
-    Box,
-    Button,
-    TextField,
-    Typography,
-} from "@mui/material";
+import { Alert, Box, Button, TextField, Typography } from "@mui/material";
 
-import {Link, useNavigate} from "react-router-dom";
-import {useApp} from "../ThemedApp";
+import { useRef, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useApp } from "../ThemedApp";
+import { postLogin } from "../libs/fetcher";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const {setAuth} = useApp();
+  const navigate = useNavigate();
 
-    return (
-        <Box>
-            <Typography variant="h3">Login</Typography>
+  const usernameInput = useRef();
+  const passwordInput = useRef();
 
-            <Alert severity="warning" sx={{ mt: 2 }}>All fields required</Alert>
+  const { setAuth } = useApp();
 
-            <form onSubmit={e => {
-                e.preventDefault();
-                setAuth(true);
-                navigate("/profile/1");
-            }}>
-                <Box
-                    sx={{ display: "flex", flexDirection: "column", gap: 1, mt: 2 }}>
-                    <TextField placeholder="Username" fullWidth/>
-                    <TextField type="password" placeholder="Password" fullWidth/>
-                    <Button type="submit" variant="contained" fullWidth>
-                        Login
-                    </Button>
-                    <Typography>If you don't have an account.<Link to="/register">Here is to Register.</Link></Typography>
-                </Box>
-            </form>
+  const [error, setError] = useState(null);
+
+  const handleSubmit = () => {
+    const username = usernameInput.current.value;
+    const password = passwordInput.current.value;
+
+    if (!username || !password) {
+      setError("username or password is required");
+      return false;
+    }
+    login.mutate({ username, password });
+  };
+
+  const login = useMutation({
+    mutationFn: async ({ username, password }) => {
+      return postLogin(username, password);
+    },
+    onError: async () => {
+      setError("Incorrect username or password");
+    },
+    onSuccess: async (result) => {
+      setAuth(result.user);
+      localStorage.setItem("token", result.token);
+      navigate("/");
+    },
+  });
+
+  return (
+    <Box>
+      <Typography variant="h3">Login</Typography>
+
+      {error && (
+        <Alert severity="warning" sx={{ mt: 2 }}>
+          {error}
+        </Alert>
+      )}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1, mt: 2 }}>
+          <TextField
+            inputRef={usernameInput}
+            placeholder="Username"
+            fullWidth
+          />
+          <TextField
+            inputRef={passwordInput}
+            type="password"
+            placeholder="Password"
+            fullWidth
+          />
+          <Button type="submit" variant="contained" fullWidth>
+            Login
+          </Button>
         </Box>
-    );
+      </form>
+    </Box>
+  );
 };
 
 export default Login;
